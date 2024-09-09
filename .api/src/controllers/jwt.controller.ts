6b43a0e7ff259d8dfd96f5ae793bfb93c5faeba8
Muzaffar Shaikh <86394234+Mohammad-Muzaffar@users.noreply.github.com>
2024-09-09 11:07:36 +0530

    Delete .api directory changed name .api to api

--- .api/src/controllers/jwt.controller.ts
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../config';
-
-export const generateJwt = (id: string) => {
-    const token = jwt.sign( id, JWT_SECRET);
-    return token;
-}
--- .api/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { urlsRouter } from './urls';
-import { userRouter } from './users';
-
-export const rootRouter = express.Router();
-
-rootRouter.use('/users',userRouter);
-rootRouter.use('/urls',urlsRouter);
